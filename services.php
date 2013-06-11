08c3946a1bc3a18fcb6e59cc67d5a6c9fa12a161
PradatiusD <danielprada2012@gmail.com>
2013-06-11 10:00:55 -0400

    PSD with new icons and original services html

---
+++ services.php
@@ -0,0 +1,33 @@
+<div class="innovators">
+<div class="wrap">
+<h1 class="entry-title" >How &amp; What We Do</h1>
+[one_half_first]We visualize your messaging and turn concepts into reality. Our method involves maintaining a network of academics, designers, marketers, photographers, and web developers. This way, our studio stays nimble and light, and our teams can be reconfigured for each project.
+
+The details of our services are marked below.[/one_half_first][one_half][/one_half]
+
+</div>
+</div>
+<div class="sectiondots"></div>
+<div class="central">
+<div class="wrap">
+<h2 style="text-align:center;">Our Primary Services</h2>
+<div class="processSort">
+
+[one_third]
+<div class="processType" id="marketing"><a class="goToSlide active" href="1"><img alt="Marketing Icon" src="http://studioissa.com/wp-content/themes/issa-studio/images/marketingicon-si.png" /><span class="SliderCaption">Marketing</span></a></div>
+[/one_third]
+
+[one_third]
+<div class="processType" id="identity"><a class="goToSlide" href="2"><img alt="Design Icon Studio Issa" src="http://studioissa.com/wp-content/themes/issa-studio/images/designicon-si.png" /><span class="SliderCaption">Design</span></a></div>
+[/one_third]
+
+[one_third]
+<div class="processType" id="artdirection"><a class="goToSlide" href="3"><img alt="Art Direction Icon Studio Issa" src="http://studioissa.com/wp-content/themes/issa-studio/images/artdirectionicon-si.png" /><span class="SliderCaption">Art Direction</span></a></div>
+[/one_third]
+
+</div>
+<div class="pagehead"></div>
+[SlideDeck2 id=491]
+
+</div>
+</div>
